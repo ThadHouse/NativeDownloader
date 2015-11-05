a35cf4c49a101dbe9bd57aeab33c6f6a0d0d5297
Thad House <thad@athometech.com>
2015-11-05 15:16:31 -0800

    Comments the native downloader

--- NativeDownloader/Program.cs
+++ NativeDownloader/Program.cs
@@ -10,6 +10,24 @@ using Newtonsoft.Json;
 
 namespace NativeDownloader
 {
+    public class ConfigInfo
+    {
+        public string Version { get; set; }
+        public string Site { get; set; }
+        public string FileName { get; set; }
+        public string OutputLocation { get; set; }
+        public string MD5 { get; set; }
+
+        public ConfigInfo(string version, string site, string filename, string outputLocation, string md5)
+        {
+            Version = version;
+            Site = site;
+            FileName = filename;
+            OutputLocation = outputLocation;
+            MD5 = md5;
+        }
+    }
+
     class Program
     {
         static int Main(string[] args)
@@ -29,25 +47,37 @@ namespace NativeDownloader
             //Load our json
             List<ConfigInfo> configInfo = JsonConvert.DeserializeObject<List<ConfigInfo>>(File.ReadAllText(args[0]));
 
-            Task<ConfigInfo[]> checkMD5 = ForEashAsync(configInfo, CheckFileMD5);
+            //Check for pre-existing correct files.
+            Task<ConfigInfo[]> checkMd5 = ForEashAsync(configInfo, CheckFileMd5);
 
-            List<ConfigInfo> configToDownload = checkMD5.Result.Where(r => r != null).ToList();
+            //Create a list of new files to download
+            List<ConfigInfo> configToDownload = checkMd5.Result.Where(r => r != null).ToList();
 
-            if (configToDownload.Count == 0) return 0;
-
-            Task<bool[]> ret = ForEashAsync(configToDownload, DownloadFile);
-
-            if (ret.Result.Any(b => b == false))
+            //If no new files to download return
+            if (configToDownload.Count == 0)
             {
-                Console.WriteLine("Failed to download a file.");
-                Console.ReadLine();
-                return 1;
+                Console.WriteLine("All files already downloaded. Returning");
+                return 0;
             }
 
-            return 0;
+            //Download all of our files
+            Task<ConfigInfo[]> ret = ForEashAsync(configToDownload, DownloadFile);
+
+            bool success = true;
+
+            //Check for any failures
+            foreach (var info in ret.Result.Where(info => info != null))
+            {
+                success = false;
+                Console.WriteLine($"Failed to download file: {info.OutputLocation}");
+            }
+
+            //Return 0 if successful, 1 if failure
+            return success ? 0 : 1;
 
         }
 
+        //Foreach loop to run all commands asyncronously and return an array of type T2
         public static Task<T2[]> ForEashAsync<T, T2>(IEnumerable<T> source, Func<T, Task<T2>> body)
         {
             return Task.WhenAll(
@@ -55,7 +85,8 @@ namespace NativeDownloader
                 select Task.Run(() => body(item)));
         }
 
-        public static async Task<ConfigInfo> CheckFileMD5(ConfigInfo info)
+        //Asyncronously check all file MD5s
+        public static async Task<ConfigInfo> CheckFileMd5(ConfigInfo info)
         {
             bool ret = await Task.Run(() => CheckFileValid(info));
             //True if file correct
@@ -63,6 +94,7 @@ namespace NativeDownloader
             return ret ? null : info;
         }
 
+        //Checks a specific file for MD5 equality
         public static bool CheckFileValid(ConfigInfo info)
         {
             string fileSum = Md5Sum(info.OutputLocation);
@@ -70,6 +102,7 @@ namespace NativeDownloader
             return fileSum != null && fileSum.Equals(info.MD5);
         }
 
+        //Grab the MD5 sum of an existing file
         private static string Md5Sum(string fileName)
         {
             byte[] fileMd5Sum = null;
@@ -99,7 +132,9 @@ namespace NativeDownloader
             return builder.ToString();
         }
 
-        public static async Task<bool> DownloadFile(ConfigInfo info)
+        // Returns null if downloaded successfully
+        //Donwload a file asyncronously
+        public static async Task<ConfigInfo> DownloadFile(ConfigInfo info)
         {
             string webUrl = Path.Combine(info.Site, info.Version, info.FileName);
             string localFileName = info.OutputLocation;
@@ -110,12 +145,12 @@ namespace NativeDownloader
                 {
                     client.Credentials = CredentialCache.DefaultNetworkCredentials;
                     await client.DownloadFileTaskAsync(new Uri(webUrl), localFileName);
-                    return true;
+                    return null;
                 }
             }
             catch (Exception)
             {
-                return false;
+                return info;
             }
         }
     }
